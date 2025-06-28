aff2b834162919cdbf28d7f2d9f8881f73f98b34
Micheal Polanski <micheal.polanski@gmail.com>
2025-06-28 08:46:43 -0400

    working on word analysis in doc

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ def get_book_text(file_path):
     return file_contents
 
 def word_analysis(file_contents):
-    analysis = get_book_text.split()
+    file_contents = "books/frankenstein.txt".split()
     count = 0
-    for char in analysis:
+    for char in file_contents:
         count += 1
     print(f"{count} words found in the document.")
 
 def main():
     print(get_book_text("books/frankenstein.txt"))
 
-word_analysis(get_book_text("books/frankenstein.txt"))
+word_analysis("books/frankenstein.txt")
 
