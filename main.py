51dd0cdc37c07935abaea485b24c2a39f9d13849
Micheal Polanski <micheal.polanski@gmail.com>
2025-06-30 06:55:08 -0400

    refactored functions to separate file, added character counting

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-# def get_book_text(file_path):
-#     with open(file_path) as f:
-#         file_contents = f.read()
-#     return file_contents
-
-def word_analysis(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-    words = file_contents.split()
-    count = 0
-    for word in words:
-        count += 1
-    return count
+from stats import word_analysis
+from stats import char_analysis
 
 def main():
-    num_words = word_analysis("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    num_words = word_analysis(book_path)
     print(f"{num_words} words found in the document")
+    print(char_analysis(book_path))
 
 main()
---
+++ stats.py
@@ -0,0 +1,19 @@
+def word_analysis(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    words = file_contents.split()
+    count = 0
+    for word in words:
+        count += 1
+    return count
+
+def char_analysis(file_path):
+    char_dict = {}
+    with open(file_path) as f:
+        file_contents = f.read()
+    for char in file_contents.lower():
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1 
+    return char_dict
